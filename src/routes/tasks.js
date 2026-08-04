1c14c315e179dd678e3be3e4ef3c19e15f207cde
AllanWerner <Allanwertabou@yahoo.com>
2026-08-04 14:02:16 +0200

    Updating middleware and road file and writng the docker-compose.yml

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
-const { validateTaskInput } = require('../middleware/errorHandler');
+const { validateTaskInput, validateTaskUpdapte  } = require('../middleware/errorHandler');
 
 // Routes CRUD avec validation
 router.get('/', taskController.getAllTasks);
 router.get('/:id', taskController.getTaskById);
 router.post('/', validateTaskInput, taskController.createTask);  
-router.put('/:id', validateTaskInput, taskController.updateTask); 
+router.put('/:id', validateTaskUpdapte, taskController.updateTask); 
 router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
